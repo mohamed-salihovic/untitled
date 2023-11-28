d5a2fb44e4e7181155b3d3913c4fb9043de72baa
mohamed-salihovic <149728941+mohamed-salihovic@users.noreply.github.com>
2023-11-28 17:34:49 +0100

    zadace

--- src/main/java/ContinueWithLabelDemo.java
+++ src/main/java/ContinueWithLabelDemo.java
@@ -8,17 +8,20 @@ public class ContinueWithLabelDemo {
         String recenica = "petar pan je pojeo panprike i zato je pan petar";
         String rijec = "pan";
         boolean rijecJesteDioRecenice = false;
-        int međa = recenica.length()-rijec.length();
+
         int kolikoPuta = 0;
-        KOLEGA: for(int i = 0; i<=međa; i++){
+        KOLEGA: for(int i = 0; i<recenica.length(); i++){
+
             for(int j = 0; j<rijec.length(); j++){
                 char slovoRecenice = recenica.charAt(i+j);
                 char slovoRijeci = rijec.charAt(j);
 
 
+
                 if(slovoRecenice != slovoRijeci)
                 {
                     continue KOLEGA;
+
 
                 }
             }
@@ -28,7 +31,8 @@ public class ContinueWithLabelDemo {
              */
             rijecJesteDioRecenice = true;
             kolikoPuta++;
-            break;
+
+            continue ;
         }
         String message = rijecJesteDioRecenice ? "rijec jeste":"riječ nije";
         System.out.println(kolikoPuta + " puta" + " " +message);
